d0218a76cd19021171ae938f53c7d34b32259354
dmitry <dmitry.kouznetsov@paxport.se>
2016-07-15 16:35:28 +0200

    another commit

--- lib/web.ex
+++ lib/web.ex
@@ -7,13 +7,13 @@ defmodule My.Web do
   def start_server do
     Plug.Adapters.Cowboy.http(__MODULE__, nil, port: 5454)
     # {:ok, _} = My.MongoPool.start_link hostname: "localhost", port: 3001, database: "meteor"
-    # {:ok, _} = My.MongoPool.start_link hostname: "ds031942.mlab.com", port: 31942, database: "taxi-hop-on", username: "macaw", password: "parrot"
+    {:ok, _} = My.MongoPool.start_link hostname: "ds031942.mlab.com", port: 31942, database: "taxi-hop-on", username: "macaw", password: "parrot"
   end
 
   get "/hello" do
     cursor = Mongo.find(My.MongoPool, "rides", %{}, [limit: 1])
-    text = cursor
-    |> BSON.decode
+
+    text = cursor |> Enum.to_list |> IO.inspect
 
     conn
     |> Plug.Conn.put_resp_content_type("text/plain")
